ee4565add78bb632d36293c7075268f673bab14f
Ubaidullah Effendi-Emjedi <mr.ueemjedi@gmail.com>
2023-03-14 00:08:31 +0200

    Refactored Locations for Editor Fonts and Icons

--- Assets/Addons/LazyJedi/Scripts/Editor/Internal/Styles/LazyEditorArt.cs
+++ Assets/Addons/LazyJedi/Scripts/Editor/Internal/Styles/LazyEditorArt.cs
@@ -15,32 +15,32 @@ namespace LazyJedi.Editors.Internal
         /// <summary>
         /// Kenney Mini Square Font
         /// </summary>
-        public const string MiniSquareFont = "Fonts/Kenney-fonts/MiniSquare";
+        public const string MiniSquareFont = "Fonts/Kenney-fonts/MiniSquare_Editor";
 
         /// <summary>
         /// MuseoModerno ExtraBold Font
         /// </summary>
-        public const string MuseoModernoExtraBoldFont = "Fonts/Google-fonts/MuseoModerno-ExtraBold";
+        public const string MuseoModernoExtraBoldFont = "Fonts/Google-fonts/MuseoModerno/MuseoModerno-ExtraBold_Editor";
 
         /// <summary>
         /// QuickSand Bold Font
         /// </summary>
-        public const string QuicksandBoldFont = "Fonts/Google-fonts/Quicksand-Bold";
+        public const string QuicksandBoldFont = "Fonts/Google-fonts/Quicksand/Quicksand-Bold_Editor";
 
         /// <summary>
         /// Quicksand Medium Font
         /// </summary>
-        public const string QuicksandSemiBoldFont = "Fonts/Google-fonts/Quicksand-SemiBold";
+        public const string QuicksandSemiBoldFont = "Fonts/Google-Fonts/Quicksand/Quicksand-SemiBold_Editor";
 
         /// <summary>
         /// Rubik Bold Font
         /// </summary>
-        public const string RubikMediumFont = "Fonts/Google-fonts/Rubik-Medium";
+        public const string RubikMediumFont = "Fonts/Google-Fonts/Rubik/Rubik-Medium_Editor";
 
         /// <summary>
         /// Rubik Semibold Font
         /// </summary>
-        public const string RubikSemiboldFont = "Fonts/Google-fonts/Rubik-Semibold";
+        public const string RubikSemiboldFont = "Fonts/Google-Fonts/Rubik/Rubik-SemiBold_Editor";
 
         #endregion
     }
