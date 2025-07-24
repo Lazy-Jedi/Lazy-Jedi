3ff29df55e46f89134ee57c5253d048cef21a0ca
Ubaidullah <22920753+Uncle-Uee@users.noreply.github.com>
2025-07-24 06:25:29 +0200

    Updated Project

--- Assets/Addons/LazyJedi/Editor/MenuItems/Packages/CustomPackages.cs
+++ Assets/Addons/LazyJedi/Editor/MenuItems/Packages/CustomPackages.cs
@@ -12,9 +12,13 @@ namespace LazyJedi.Editors.MenuItems
 
         private static AddRequest _request;
 
-        private const string _newtonsoft = "com.unity.nuget.newtonsoft-json";
-        private const string _uniTask = "https://github.com/Cysharp/UniTask.git?path=src/UniTask/Assets/Plugins/UniTask";
-        private const string _nuGet = "https://github.com/GlitchEnzo/NuGetForUnity.git?path=/src/NuGetForUnity";
+        private const string _newtonsoftPackage = "com.unity.nuget.newtonsoft-json";
+        private const string _nuGetUrl = "https://github.com/GlitchEnzo/NuGetForUnity";
+        private const string _nuGetPackage = "https://github.com/GlitchEnzo/NuGetForUnity.git?path=/src/NuGetForUnity";
+        private const string _uniTaskUrl = "https://github.com/Cysharp/UniTask";
+        private const string _uniTaskPackage = "https://github.com/Cysharp/UniTask.git?path=src/UniTask/Assets/Plugins/UniTask";
+        private const string zLinqURL = "https://github.com/Cysharp/ZLinq?tab=readme-ov-file#unity";
+        private const string _zLinqPackage = "https://github.com/Cysharp/ZLinq.git?path=src/ZLinq.Unity/Assets/ZLinq.Unity";
 
         #endregion
 
@@ -23,24 +27,32 @@ namespace LazyJedi.Editors.MenuItems
         [MenuItem("Lazy-Jedi/Import Packages/Newtonsoft.Json", priority = 120)]
         public static void ImportNewtonsoftJson()
         {
-            _request = Client.Add(_newtonsoft);
+            _request = Client.Add(_newtonsoftPackage);
             EditorApplication.update += Progress;
         }
         
-        [MenuItem("Lazy-Jedi/Import Packages/UniTask", priority = 121)]
-        public static void ImportUniTask()
-        {
-            _request = Client.Add(_uniTask);
-            EditorApplication.update += Progress;
-            Application.OpenURL("https://github.com/Cysharp/UniTask");
-        }
-        
-        [MenuItem("Lazy-Jedi/Import Packages/NugetForUnity", priority = 122)]
+        [MenuItem("Lazy-Jedi/Import Packages/NugetForUnity", priority = 121)]
         public static void ImportNuGet()
         {
-            _request = Client.Add(_nuGet);
+            _request = Client.Add(_nuGetPackage);
             EditorApplication.update += Progress;
-            Application.OpenURL("https://github.com/GlitchEnzo/NuGetForUnity");
+            Application.OpenURL(_nuGetUrl);
+        }
+        
+        [MenuItem("Lazy-Jedi/Import Packages/UniTask", priority = 122)]
+        public static void ImportUniTask()
+        {
+            _request = Client.Add(_uniTaskPackage);
+            EditorApplication.update += Progress;
+            Application.OpenURL(_uniTaskUrl);
+        }
+        
+        [MenuItem("Lazy-Jedi/Import Packages/ZLinq", priority = 123)]
+        public static void ImportZLinq()
+        {
+            _request = Client.Add(_zLinqPackage);
+            EditorApplication.update += Progress;
+            Application.OpenURL(zLinqURL);
         }
 
         #endregion
